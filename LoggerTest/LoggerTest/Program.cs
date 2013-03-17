c6257203147d2f8ecca91c06d25800e2e88a632d
hmuronaka <capriccio27@gmail.com>
2013-03-17 23:22:21 +0900

    テスト用プロジェクトの追加

---
+++ LoggerTest/LoggerTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using h.mu.Logger;
+
+namespace LoggerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RollingFileWriter writer = new RollingFileWriter(@"..\logdir", "test", "log", 1024, 2);
+            writer.open();
+            for (int i = 0; i < 100; i++)
+            {
+                writer.writeLine("0123456789");
+            }
+            writer.close();
+        }
+    }
+}
